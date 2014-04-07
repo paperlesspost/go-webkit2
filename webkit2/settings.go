34931241a687c525a8dd377aab8f459c14ec2df7
Aaron Quint <aaron@quirkey.com>
2014-04-07 15:02:21 -0400

    Add setting method for enabling Webgl

--- webkit2/settings.go
+++ webkit2/settings.go
@@ -42,3 +42,11 @@ func (s *Settings) SetAutoLoadImages(autoLoad bool) {
 func (s *Settings) SetUserAgentWithApplicationDetails(appName, appVersion string) {
 	C.webkit_settings_set_user_agent_with_application_details(s.settings, (*C.gchar)(C.CString(appName)), (*C.gchar)(C.CString(appVersion)))
 }
+
+// SetEnableWebgl sets the "enable-webgl" property.
+//
+// See also: webkit_settings_set_enable_webgl at
+// http://webkitgtk.org/reference/webkit2gtk/unstable/WebKitSettings.html#webkit-settings-set-enable-webgl 
+func (s *Settings) SetEnableWebgl(enabled bool) {
+	C.webkit_settings_set_enable_webgl(s.settings, gboolean(enabled))
+}
